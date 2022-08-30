a7253118674bc3ccc14f54b82ef7a1b5af880783
Chris Pettyjohn <chrismmpettyjohn>
2022-08-29 22:46:39 -0500

    Add support for online users grid

---
+++ apps/web/src/components/online-users-grid/OnlineUsersGrid.tsx
@@ -0,0 +1,48 @@
+import {Link} from 'wouter';
+import gql from 'graphql-tag';
+import React, {useEffect} from 'react';
+import {UserWire} from '@imagine-cms/types';
+import {useRunQuery} from '../../graphql/run-query';
+
+const FETCH_ONLINE_USERS = gql`
+    query {
+        users {
+            id,
+            username,
+            look,
+        }
+    }
+`
+
+export function OnlineUsersGrid() {
+  const {runQuery, loading, data} = useRunQuery<{users: UserWire[]}>(FETCH_ONLINE_USERS)
+
+  useEffect(() => {
+    runQuery();
+  }, []);
+
+  if (loading) {
+    return <i className="fa fa-spinner fa-spin" />
+  }
+
+  return (
+    <>
+      <h4 className="mb-3">Online Users</h4>
+      {
+        loading && <i className="fa fa-spinner fa-spin" />
+      }
+      {
+        data?.users?.map((user, userIndex) => (
+          <div className={`avatar ${userIndex % 2 ? 'dark-gray' : 'light-gray'}`} key={`online_user_${user.id}`}>
+            <Link to={`/profile/${user.username}`}>
+              <img
+                src={`https://imager.habboon.pw?figure=${user.look}&size=m&direction=3&head_direction=3&gesture=sml&headonly=1`}
+                alt={user.username} data-toggle="tooltip" data-placement="top" title="" loading="lazy"
+                data-original-title={user.username} />
+            </Link>
+          </div>
+        ))
+      }
+    </>
+  )
+}
--- apps/web/src/components/site-footer/SiteFooter.tsx
+++ apps/web/src/components/site-footer/SiteFooter.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {OnlineUsersGrid} from '../online-users-grid/OnlineUsersGrid';
 
 export function SiteFooter() {
   return (
@@ -8,90 +9,7 @@ export function SiteFooter() {
         <div className="container">
           <div className="row">
             <div className="col-lg-6 col-12 d-lg-block d-none">
-              <h4 className="mb-3">Online Users</h4>
-              <div className="avatar  light-gray ">
-                <img
-                  src="https://imager.habboon.pw?figure=ha-1009-93.ch-215-106.hd-180-1.ca-1813-63.hr-828-37.lg-285-102.sh-295-1408.&amp;size=m&amp;direction=3&amp;head_direction=3&amp;gesture=sml&amp;headonly=1"
-                  alt="deez_nuts" data-toggle="tooltip" data-placement="top" title="" loading="lazy"
-                  data-original-title="deez_nuts" />
-              </div>
-              <div className="avatar  dark-gray ">
-                <img
-                  src="https://imager.habboon.pw?figure=ch-9120844-110.hr-5703-1343-35.sh-8335-110.hd-629-1.he-3082-98.ca-8529-71-71.lg-5934-110.cc-3066-110.&amp;size=m&amp;direction=3&amp;head_direction=3&amp;gesture=sml&amp;headonly=1"
-                  alt="buttercup3" data-toggle="tooltip" data-placement="top" title="" loading="lazy"
-                  data-original-title="buttercup3" />
-              </div>
-              <div className="avatar  light-gray ">
-                <img
-                  src="https://imager.habboon.pw?figure=sh-300-92.lg-3290-1336.hd-180-1.ch-235-68.hr-3278-45-40.fa-3344-91.&amp;size=m&amp;direction=3&amp;head_direction=3&amp;gesture=sml&amp;headonly=1"
-                  alt="whydietoday" data-toggle="tooltip" data-placement="top" title="" loading="lazy"
-                  data-original-title="buttercup3" />
-              </div>
-              <div className="avatar  dark-gray ">
-                <img
-                  src="https://imager.habboon.pw?figure=hr-828-1342.lg-270-63.sh-295-92.ch-215-88.hd-205-1.ha-4294-1408.ca-3885-1320-1320.&amp;size=m&amp;direction=3&amp;head_direction=3&amp;gesture=sml&amp;headonly=1"
-                  alt="dog" data-toggle="tooltip" data-placement="top" title="" loading="lazy"
-                  data-original-title="buttercup3" />
-              </div>
-              <div className="avatar  light-gray ">
-                <img
-                  src="https://imager.habboon.pw?figure=hd-95945-2.fa-4931-1329-68.ch-3487-79-79.hr-4182-1394-1394.lg-4066-79.he-3958-79.&amp;size=m&amp;direction=3&amp;head_direction=3&amp;gesture=sml&amp;headonly=1"
-                  alt="SofiaTorres" data-toggle="tooltip" data-placement="top" title="" loading="lazy"
-                  data-original-title="buttercup3" />
-              </div>
-              <div className="avatar  dark-gray ">
-                <img
-                  src="https://imager.habboon.pw?figure=ca-1801-110.cc-3008-110-110.sh-53415682-110.hr-25573-1405.lg-3192-110.ch-3046-110.hd-625-10.&amp;size=m&amp;direction=3&amp;head_direction=3&amp;gesture=sml&amp;headonly=1"
-                  alt="Mechikabura" data-toggle="tooltip" data-placement="top" title="" loading="lazy"
-                  data-original-title="buttercup3" />
-              </div>
-              <div className="avatar  light-gray ">
-                <img
-                  src="https://imager.habboon.pw?figure=hd-600-2.ch-8374-110-1408.hr-7326-45-45.he-3070-92.fa-4931-73-73.ca-3973-110-92.sh-7239-92.lg-3933-1329.&amp;size=m&amp;direction=3&amp;head_direction=3&amp;gesture=sml&amp;headonly=1"
-                  alt="VivianaCastillo" data-toggle="tooltip" data-placement="top" title="" loading="lazy"
-                  data-original-title="buttercup3" />
-              </div>
-              <div className="avatar  dark-gray ">
-                <img
-                  src="https://imager.habboon.pw?figure=hd-180-1366.lg-280-1408.ha-1002-1408.sh-305-1408.he-3229-80.&amp;size=m&amp;direction=3&amp;head_direction=3&amp;gesture=sml&amp;headonly=1"
-                  alt="kicc" data-toggle="tooltip" data-placement="top" title="" loading="lazy"
-                  data-original-title="buttercup3" />
-              </div>
-              <div className="avatar  light-gray ">
-                <img
-                  src="https://imager.habboon.pw?figure=hd-600-1.hr-7326-1404-31.lg-3174-92-92.ch-4392-92-71.he-3329-77-71.sh-3064-1326.ca-3973-77-92.fa-3276-1328.wa-8555-92.&amp;size=m&amp;direction=3&amp;head_direction=3&amp;gesture=sml&amp;headonly=1"
-                  alt=".Mila." data-toggle="tooltip" data-placement="top" title="" loading="lazy"
-                  data-original-title="buttercup3" />
-              </div>
-              <div className="avatar  dark-gray ">
-                <img
-                  src="https://imager.habboon.pw?figure=hr-9534-31.hd-605-2.ch-660-97.lg-8472-1325.sh-3206-1320.he-3155-1325.fa-3276-72.&amp;size=m&amp;direction=3&amp;head_direction=3&amp;gesture=sml&amp;headonly=1"
-                  alt="petutotita" data-toggle="tooltip" data-placement="top" title="" loading="lazy"
-                  data-original-title="buttercup3" />
-              </div>
-              <div className="avatar  light-gray ">
-                <img
-                  src="https://imager.habboon.pw?figure=hr-4363-1403-1403.lg-827-1409.hd-615-17.sh-908-92.fa-1202-1409.ch-3135-67.he-3833-92.cc-3744-1409-1325.ha-50024-1408.ca-3828-89-1325.&amp;size=m&amp;direction=3&amp;head_direction=3&amp;gesture=sml&amp;headonly=1"
-                  alt="U" data-toggle="tooltip" data-placement="top" title="" loading="lazy" data-original-title="U" />
-              </div>
-              <div className="avatar  dark-gray ">
-                <img
-                  src="https://imager.habboon.pw?figure=hd-3096-10.ch-4067-110-92.hr-4117-1342-1342.lg-4083-1408.sh-3354-110-92.&amp;size=m&amp;direction=3&amp;head_direction=3&amp;gesture=sml&amp;headonly=1"
-                  alt="Camomile" data-toggle="tooltip" data-placement="top" title="" loading="lazy"
-                  data-original-title="buttercup3" />
-              </div>
-              <div className="avatar  light-gray ">
-                <img
-                  src="https://imager.habboon.pw?figure=hd-180-17.lg-285-63.ch-210-63.sh-290-92.he-3557-1408.hr-100-40.&amp;size=m&amp;direction=3&amp;head_direction=3&amp;gesture=sml&amp;headonly=1"
-                  alt="khaki" data-toggle="tooltip" data-placement="top" title="" loading="lazy"
-                  data-original-title="buttercup3" />
-              </div>
-              <div className="avatar  dark-gray ">
-                <img
-                  src="https://imager.habboon.pw?figure=ha-3145-110-68.ca-1816-63.cp-233287-1408.he-1608-1408.ch-255-110.hd-209-1.cc-3420-1408.wa-3263-63-1408.sh-9546-1408.ea-1404-110.lg-3596-110-1408.hr-3163-61.&amp;size=m&amp;direction=3&amp;head_direction=3&amp;gesture=sml&amp;headonly=1"
-                  alt="Hysteria" data-toggle="tooltip" data-placement="top" title="" loading="lazy"
-                  data-original-title="buttercup3" />
-              </div>
+             <OnlineUsersGrid />
             </div>
             <div className="col-lg-3 col-6">
               <h4 className="mb-3">Help</h4>
