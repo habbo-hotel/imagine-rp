1b9b37a4224acf5deb2ec21d8d02510d72d1d06d
Chris Pettyjohn <chrismmpettyjohn>
2022-09-16 22:43:21 -0500

    remove unused code from modern flat

--- apps/modern-flat-web/src/components/leaderboards-card/LeaderboardsCard.types.ts
+++
@@ -1,8 +0,0 @@
-import { DocumentNode } from 'graphql';
-import {UserWire} from '@imagine-cms/types';
-
-export interface LeaderboardsCardProps {
-  title: string;
-  value(user: UserWire): string;
-  query: DocumentNode;
-}
--- apps/modern-flat-web/src/screens/me-screen/MeScreen.tsx
+++ apps/modern-flat-web/src/screens/me-screen/MeScreen.tsx
@@ -1,20 +1,5 @@
 import React from 'react';
-import {UserContainer} from './user-container/UserContainer';
-import {SocialMediaButtons} from '../../components/social-media/SocialMediaButtons';
-import {LatestArticlesSlider} from '../../components/latest-articles-slider/LatestArticlesSlider';
 
 export function MeScreen() {
-  return (
-    <main className="position-relative container justify-content-center py-4">
-      <div className="row">
-        <div className="col-lg-9 col-12">
-          <UserContainer />
-          <LatestArticlesSlider />
-        </div>
-        <div className="col-lg-3 col-12">
-          <SocialMediaButtons />
-        </div>
-      </div>
-    </main>
-  )
+  return <p>me</p>
 }
