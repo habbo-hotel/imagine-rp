ebbc7b1c79818c5e2bdd7b7e2623cd7d80d724a5
Chris Pettyjohn <chrismmpettyjohn>
2022-10-02 19:40:56 -0500

    stash changes for now

--- apps/levlo-web/src/screens/login-screen/LoginScreen.tsx
+++ apps/levlo-web/src/screens/login-screen/LoginScreen.tsx
@@ -23,42 +23,75 @@ export function LoginScreen() {
 
   return (
     <GuestGuard redirect>
-      <div className="login-page">
-        <div className="hero">
-          <div className="hotel"></div>
-        </div>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-12">
+            <div className="content-box">
+              <div className="login-footer">
+                <div className="logo" />
+                <div className="username-position">
+                  <label htmlFor="username">Username:</label>
+                  <input type="text" name="username" id="username" />
+                </div>
+                <div className="password-position">
+                  <label htmlFor="password">Password:</label>
+                  <input type="text" name="password" id="password" />
+                </div>
 
-        <div id="header-content" style={{background: 'white'}}>
-          <div className="container">
-            <div className="row">
-              <div className="col-md-12">
-                <div className="logo"></div>
-                <div className="online-count"><b>0</b> users online</div>
+                <div className="button-position">
+                  <button>Login</button>
+                </div>
               </div>
+              <div className="welcome-message">
+                <h2>Welcome to {config!.siteName},</h2>
+                <p>
+                  Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut
+                  labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores
+                  et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem
+                  ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et
+                  dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea
+                  rebum. Stet clita kasd
+                </p>
+              </div>
+
+              <div className="onlineBubble">
+                <b>0</b>
+                users online
+              </div>
+
+              <a href="register.php" className="register-button">
+                <span className="big_text">Kostenlos</span>
+                <span className="small_text">mitmachen</span>
+              </a>
+              <div className="image" />
             </div>
           </div>
         </div>
-        <div className="container">
-          <div className="row">
-            <div className="col-md-5">
-              <div className="login-position">
-                <h2>Sign In</h2>
-                <form onSubmit={onLogin}>
-                  <label htmlFor="login-username">Username</label>
-                  <input type="text" name="login-username" id="login-username" value={username} onChange={e => setUsername(e?.target?.value ?? '')} />
-                  <label htmlFor="login-password">Password</label>
-                  <input type="password" name="login-password" id="login-password" value={password} onChange={e => setPassword(e?.target?.value ?? '')} />
-                  <button className="btn big green login-button" type="submit" disabled={isDisabled}>Login</button>
-                </form>
-                <div className="clear"></div>
-                <div className="row">
-                  <div className="col-md-12">
-                    <p>or</p>
-                  </div>
-                  <div className="col-md-12">
-                    <Link to="/register" className="btn big orange register-button">Register for free</Link>
-                  </div>
-                </div>
+
+        <div className="row">
+          <div className="col-md-4">
+            <div className="info-box">
+              <div className="image" style={{backgroundImage: 'url(/img/frontpage/habbo_credits.png)' }} />
+              <div className="desc">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
+                tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
+              </div>
+            </div>
+          </div>
+
+          <div className="col-md-4">
+            <div className="info-box">
+              <div className="image" style={{backgroundImage: 'url(/img/frontpage/build_room.png)' }} />
+              <div className="desc">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
+                tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
+              </div>
+            </div>
+          </div>
+
+          <div className="col-md-4">
+            <div className="info-box">
+              <div className="image" style={{backgroundImage: 'url(/img/frontpage/play_events.png)' }} />
+              <div className="desc">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
+                tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
               </div>
             </div>
           </div>
