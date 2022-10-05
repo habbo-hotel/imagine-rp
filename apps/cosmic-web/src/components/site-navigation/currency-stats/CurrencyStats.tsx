a66171c485e2bcc4cf9c4adc7eb827c7596c81e1
Chris Pettyjohn <chrismmpettyjohn>
2022-10-04 19:00:53 -0500

    Update env example to use ws

--- apps/cosmic-web/src/components/site-navigation/currency-stats/CurrencyStats.tsx
+++ apps/cosmic-web/src/components/site-navigation/currency-stats/CurrencyStats.tsx
@@ -8,14 +8,14 @@ export function CurrencyStats() {
       <div className="user-bar">
         <div className="icon credits" />
         <div className="item-column">
-          <div className="item-text amount bold">{Number(session.credits!).toLocaleString()}</div>
+          <div className="item-text amount bold">{Number(session!.credits!).toLocaleString()}</div>
           <div className="item-text">credits</div>
         </div>
       </div>
       <div className="user-bar">
         <div className="icon duckets" />
         <div className="item-column">
-          <div className="item-text amount bold">{Number(session.activityPoints!).toLocaleString()}</div>
+          <div className="item-text amount bold">{Number(session!.activityPoints!).toLocaleString()}</div>
           <div className="item-text">duckets</div>
         </div>
       </div>
