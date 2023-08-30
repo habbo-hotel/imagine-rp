358bedc8edea7e6ae7c8b69ef4a16218d74a9c35
chrismpettyjohn <chrismpettyjohn@gmail.com>
2023-08-30 19:08:19 -0400

    enhance: Add friend container

--- apps/imagine-web/src/screens/me-screen/my-friends/MyFriends.styled.ts
+++ apps/imagine-web/src/screens/me-screen/my-friends/MyFriends.styled.ts
@@ -3,12 +3,45 @@ import styled from "styled-components";
 export const MyFriendsContainer = styled.div`
   background: ${({ theme }) => theme.color.s20};
   border-radius: ${({ theme }) => theme.radius.oneUnit};
-  display: flex;
-  flex: 1;
-  gap: ${({ theme }) => theme.space.twoUnits};
   padding: ${({ theme }) => theme.space.twoUnits};
-  
+
   h1 {
     font-size: ${({ theme }) => theme.fontSize.twoUnits};
   }
+`
+
+export const MyFriendsGrid = styled.div`
+  display: flex;
+  flex: 1;
+  width: 100%;
+`
+
+export const MyFriendUserContainer = styled.div`
+  background: ${({ theme }) => theme.color.s30};
+  border-radius: ${({ theme }) => theme.radius.oneUnit};
+  color: ${({ theme }) => theme.color.s20};
+  display: flex;
+  flex-direction: column;
+  position: relative;
+  height: 150px;
+  overflow: hidden;
+  width: 150px;
+`
+
+export const MyFriendAvatarContainer = styled.img`
+  position: absolute;
+  top: 0px;
+  left: 20px;
+  width: 110px;
+`
+
+export const MyFriendInformationContainer = styled.div`
+  background: black;
+  display: flex;
+  flex: 1;
+  justify-content: center;
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  width: 100%;
 `
--- apps/imagine-web/src/screens/me-screen/my-friends/MyFriends.tsx
+++ apps/imagine-web/src/screens/me-screen/my-friends/MyFriends.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
-import { MyFriendsContainer } from './MyFriends.styled';
+import { MyFriendAvatarContainer, MyFriendInformationContainer, MyFriendUserContainer, MyFriendsContainer, MyFriendsGrid } from './MyFriends.styled';
 
 export function MyFriends() {
   return (
     <MyFriendsContainer>
       <h1>My Friends</h1>
+      <MyFriendsGrid>
+        <MyFriendUserContainer>
+          <MyFriendAvatarContainer src="https://images.habbox.fr/?figure=hr-802-37.hd-185-1.ch-804-82.lg-280-73.sh-3068-1408-1408.wa-2001" />
+          <MyFriendInformationContainer>
+            <h3>ElChris</h3>
+          </MyFriendInformationContainer>
+        </MyFriendUserContainer>
+      </MyFriendsGrid>
     </MyFriendsContainer>
   )
 }
