50af92822f42af9f35e68e9d1d481174f340e8ef
chrismpettyjohn <chrismpettyjohn@gmail.com>
2023-09-25 21:04:00 -0400

    bugfix: Resolve build issues

--- apps/imagine-web/src/const.ts
+++ apps/imagine-web/src/const.ts
@@ -8,13 +8,10 @@ export const MOCK_USER: UserWire = {
   vipPoints: 0,
   accountCreatedAt: 0,
   motto: 'Join or create an account today',
-  gameSSO: '',
   rankID: -1,
   activityPoints: 0,
   look: '-',
   gender: UserGender.Male,
   onlineStatus: UserOnlineStatus.Offline,
-  ipLast: '-1',
-  ipRegistered: '-1',
   homeRoomID: -1,
 }
