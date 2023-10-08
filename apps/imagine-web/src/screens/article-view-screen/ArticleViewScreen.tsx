af72ba0afb67fe539404f9768b0f9df7f90d7a65
chrismpettyjohn <chrismpettyjohn@gmail.com>
2023-10-08 02:37:47 -0400

    design: Article view page is a grid

--- apps/imagine-web/src/screens/article-view-screen/ArticleViewScreen.tsx
+++ apps/imagine-web/src/screens/article-view-screen/ArticleViewScreen.tsx
@@ -1,6 +1,7 @@
 import { useRoute } from 'wouter';
 import React, { useEffect, useState } from 'react';
 import { useArticleFetchOne } from '@imagine-cms/client';
+import { GridLargeSmall } from '../../components/grid/Grid.remix';
 import { ArticleCommentsCard } from './article-comments-card/ArticleCommentsCard';
 import { LongUserContainer } from '../../components/long-user-container/LongUserContainer';
 import { ArticlePostCommentCard } from './article-post-comment-card/ArticlePostCommentCard';
@@ -22,25 +23,27 @@ export function ArticleViewScreen() {
   }, [articleID]);
 
   return (
-    <>
-      <ArticleHeaderContainer>
-        <ArticleHeaderBackground src={fetchArticle.data?.imageURL} />
-        <ArticleHeaderOverlay />
-        <ArticleHeaderContent>
-          <h1>{fetchArticle.data?.name}</h1>
-          <p>{fetchArticle.data?.description}</p>
-        </ArticleHeaderContent>
-      </ArticleHeaderContainer>
-      <ArticleContentElement>
-        <ArticleContentContainer>
-          <div dangerouslySetInnerHTML={{ __html: fetchArticle.data?.content ?? '' }} />
-          {fetchArticle.data?.user && <LongUserContainer user={fetchArticle.data.user as any} />}
-        </ArticleContentContainer>
-      </ArticleContentElement>
-      <br />
-      <ArticleCommentsCard articleID={articleID} key={`article_comments_${key}`} />
-      <br />
-      <ArticlePostCommentCard articleID={articleID} onPost={onReloadArticleResources} />
-    </>
+    <GridLargeSmall>
+      <div>
+        <ArticleHeaderContainer>
+          <ArticleHeaderBackground src={fetchArticle.data?.imageURL} />
+          <ArticleHeaderOverlay />
+          <ArticleHeaderContent>
+            <h1>{fetchArticle.data?.name}</h1>
+            <p>{fetchArticle.data?.description}</p>
+          </ArticleHeaderContent>
+        </ArticleHeaderContainer>
+        <ArticleContentElement>
+          <ArticleContentContainer>
+            <div dangerouslySetInnerHTML={{ __html: fetchArticle.data?.content ?? '' }} />
+            {fetchArticle.data?.user && <LongUserContainer user={fetchArticle.data.user as any} />}
+          </ArticleContentContainer>
+        </ArticleContentElement>
+      </div>
+      <div style={{ display: 'flex', flex: 1, flexDirection: 'column', gap: 16 }}>
+        <ArticleCommentsCard articleID={articleID} key={`article_comments_${key}`} />
+        <ArticlePostCommentCard articleID={articleID} onPost={onReloadArticleResources} />
+      </div>
+    </GridLargeSmall>
   )
 }
