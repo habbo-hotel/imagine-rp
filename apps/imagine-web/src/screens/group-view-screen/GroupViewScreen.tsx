2b08821446da687c1438173d8eccf346bcca620f
chrismpettyjohn <chrismpettyjohn@gmail.com>
2023-10-08 12:27:48 -0400

    enhance: add mock views for loading states on groups page

--- apps/imagine-web/src/screens/group-view-screen/GroupViewScreen.tsx
+++ apps/imagine-web/src/screens/group-view-screen/GroupViewScreen.tsx
@@ -2,11 +2,13 @@ import { Link, useRoute } from 'wouter';
 import React, { useEffect } from 'react';
 import { Card } from '../../components/card/Card';
 import { useGroupFetchOne } from '@imagine-cms/client';
-import { LoadingMessage } from '../../components/loading-message/LoadingMessage';
 import { GroupMembersCard } from '../../components/group-members-card/GroupMembersCard';
 import { LargeGroupContainer } from '../../components/large-group-container/LargeGroupContainer';
 import { RoomGridContainerLazy } from '../../components/room-grid-container/RoomGridContainerLazy';
+import { RoomGridContainerMock } from '../../components/room-grid-container/RoomGridContainerMock';
+import { LargeGroupContainerMock } from '../../components/large-group-container/LargeGroupContainer.mock';
 import { SmallUserProfileContainerLazy } from '../../components/small-user-profile-container/SmallUserProfileContainerLazy';
+import { SmallUserProfileContainerMock } from '../../components/small-user-profile-container/SmallUserProfileContainerMock';
 
 export function GroupViewScreen() {
   const [_, params] = useRoute<{ groupID: string }>('/groups/:groupID');
@@ -24,27 +26,18 @@ export function GroupViewScreen() {
     <>
       <h1><Link to="/groups"><i className="fa fa-caret-left" style={{ cursor: 'pointer', marginRight: 8 }} /></Link>Viewing Group:</h1>
       {
-        fetchGroup.loading && (
-          <LoadingMessage>
-            Loading group
-          </LoadingMessage>
-        )
-      }
-      {
-        group && <LargeGroupContainer group={group} />
+        group ? <LargeGroupContainer group={group} /> : <LargeGroupContainerMock />
       }
       <br />
       <div style={{ display: 'grid', gridTemplateColumns: '1fr 2fr', gap: 16 }}>
         <div>
           <Card header="Owned By">
-            {fetchGroup.loading && <i className="fa fa-spinner fa-spin" />}
-            {group && <SmallUserProfileContainerLazy userID={group.userID} />}
+            {group ? <SmallUserProfileContainerLazy userID={group.userID} /> : <SmallUserProfileContainerMock />}
           </Card>
           <br />
           <Card header="Home Room">
-            {fetchGroup.loading && <i className="fa fa-spinner fa-spin" />}
             {
-              group && <RoomGridContainerLazy roomID={group.roomID} />
+              group ? <RoomGridContainerLazy roomID={group.roomID} /> : <RoomGridContainerMock />
             }
           </Card>
         </div>
--- apps/imagine-web/src/screens/imagine-screen/ImagineScreen.tsx
+++ apps/imagine-web/src/screens/imagine-screen/ImagineScreen.tsx
@@ -15,8 +15,8 @@ export function ImagineScreen() {
             <img src="/img/imagine.png" height={250} width={250} loading="lazy" />
           </ImagineLogoContainer>
           <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center' }}>
-            <p>Crafted with precision using the latest technologies like NodeJS, NestJS, ReactJS, Vite, GraphQL, and Websockets, it's not just a CMS; it's a game-changer for retros everywhere.</p>
-            <p>Imagine offers the unbeatable combination of top-tier performance, blazing-fast speeds, and a development experience that's second to none. With Imagine, you're not just getting a software; you're unlocking a world of possibilities.</p>
+            <p>Crafted with precision using the latest technology, it's not just a CMS; it's a game-changer for retros everywhere.</p>
+            <p>Imagine offers the unbeatable combination of top-tier performance, blazing-fast speeds, and a development experience that's second to none.</p>
             <p>Solely designed and developed from the ground up by <b>LeChris</b></p>
             <p><b>Version:</b>{config?.softwareVersion ?? 'N/A'}</p>
           </div>
