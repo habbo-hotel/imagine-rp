608a4327e9f432c548f67269a80532bb23fdc4b6
chrismpettyjohn <chrismpettyjohn@gmail.com>
2023-10-07 22:34:22 -0400

    enhance: Add rank color and badge to username

--- apps/imagine-web/src/components/user-profile-container/UserProfileContainer.tsx
+++ apps/imagine-web/src/components/user-profile-container/UserProfileContainer.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { UserProfileContainerProps } from './UserProfileContainer.types';
 import { UserBadgeContainerGrid } from '../user-badge-container-grid/UserBadgeContainerGrid';
 import { AvatarContainer, InformationContainer, UserProfileContainerContent, UserProfileContainerElement } from './UserProfileContainer.styled';
+import { Badge } from '../badge/Badge';
 
 export function UserProfileContainer({ user }: UserProfileContainerProps) {
   return (
@@ -10,7 +11,13 @@ export function UserProfileContainer({ user }: UserProfileContainerProps) {
         <AvatarContainer src={`https://imager.habboon.pw/?figure=${user.look}&direction=2&head_direction=3&gesture=sml&action=wav&size=l`} />
         <InformationContainer>
           <div>
-            <h2 className="notranslate">{user.username}</h2>
+
+            <div style={{ display: 'flex', gap: 8 }}>
+              <Badge badge={{ code: user.rank.badgeCode }} style={{ height: 45, marginTop: 10 }} />
+              <h2 className="notranslate" style={{ color: user.rank.backgroundColor }}>
+                {user.username}
+              </h2>
+            </div>
             {user.motto && <span className="notranslate">"{user.motto}"</span>}
           </div>
           <br />
--- libs/web/src/context/theme/ThemeProvider.tsx
+++ libs/web/src/context/theme/ThemeProvider.tsx
@@ -1,23 +1,11 @@
-import {themeContext} from './ThemeContext';
-import React, {useEffect, useState} from 'react';
-import {defaultThemeContextInterface, ThemeContext, ThemeContextProviderProps} from './Theme.types';
+import React, { useState } from 'react';
+import { themeContext } from './ThemeContext';
+import { defaultThemeContextInterface, ThemeContext, ThemeContextProviderProps } from './Theme.types';
 
-export function ThemeContextProvider({children}: ThemeContextProviderProps) {
+export function ThemeContextProvider({ children }: ThemeContextProviderProps) {
   const [state, setState] = useState<ThemeContext>(
     defaultThemeContextInterface
   );
-
-  useEffect(() => {
-    const clientSet = document.body.classList.contains('client-modal');
-    if (state.showClient && !clientSet) {
-      document.body.classList.add('client-modal');
-      return;
-    }
-
-    if (clientSet) {
-      document.body.classList.remove('client-modal');
-    }
-  }, [state.showClient]);
 
   function setStore(changes: Partial<ThemeContext>): void {
     setState(_ => ({
@@ -27,7 +15,7 @@ export function ThemeContextProvider({children}: ThemeContextProviderProps) {
   }
 
   return (
-    <themeContext.Provider value={{...state, setTheme: setStore}}>
+    <themeContext.Provider value={{ ...state, setTheme: setStore }}>
       {children}
     </themeContext.Provider>
   );
