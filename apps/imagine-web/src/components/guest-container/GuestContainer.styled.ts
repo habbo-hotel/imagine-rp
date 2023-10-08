7c6def2b5019ae6bc8c1cf320c6b048a92eaa206
chrismpettyjohn <chrismpettyjohn@gmail.com>
2023-10-07 22:25:44 -0400

    design: Clean up light theme colors

--- apps/imagine-web/src/components/guest-container/GuestContainer.styled.ts
+++ apps/imagine-web/src/components/guest-container/GuestContainer.styled.ts
@@ -1,8 +1,9 @@
 import styled from "styled-components";
 
 export const GuestContainerElement = styled.div`
-  background: ${({ theme }) => theme.color.s30};
+  background: ${({ theme }) => theme.color.s20};
   border-radius: ${({ theme }) => theme.radius.twoUnits};
+  border: ${({ theme }) => `2px solid ${theme.color.s30}`};
   padding: ${({ theme }) => theme.space.twoUnits};
   .guest-pic {
     border-radius: ${({ theme }) => theme.radius.twoUnits};
--- apps/imagine-web/src/components/input/Input.styled.ts
+++ apps/imagine-web/src/components/input/Input.styled.ts
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 export const InputElement = styled.input`
-  background: ${({ theme }) => theme.color.s20};
+  background: ${({ theme }) => theme.color.s30};
   border:${({ theme }) => `2px solid ${theme.color.s40}`};
   border-radius: ${({ theme }) => theme.radius.oneUnit};
   color: ${({ theme }) => theme.color.brand};
