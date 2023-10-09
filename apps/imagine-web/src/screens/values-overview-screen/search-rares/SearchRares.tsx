899639c64610919fd43b1e4e40b300eb0412696f
chrismpettyjohn <chrismpettyjohn@gmail.com>
2023-10-08 23:11:35 -0400

    feature: rare search can store params in url

--- apps/imagine-web/src/screens/values-overview-screen/search-rares/SearchRares.tsx
+++ apps/imagine-web/src/screens/values-overview-screen/search-rares/SearchRares.tsx
@@ -1,12 +1,35 @@
-import { useLocation } from 'wouter';
 import Select from 'react-select/async'
-import React, { useState } from 'react';
-import { FurnitureFragment, useFurnitureFetchMany } from '@imagine-cms/client';
+import React, { useEffect, useMemo } from 'react';
+import { useLocation, useRoute } from 'wouter';
+import { useFurnitureFetchMany } from '@imagine-cms/client';
 import { FurnitureIcon } from '../../../components/furniture-icon/FurnitureIcon';
 
 export function SearchRares() {
   const [, setLocation] = useLocation();
+  const [, params] = useRoute<{ furniID?: string }>('/values/:furniID');
   const fetchFurniture = useFurnitureFetchMany();
+
+  useEffect(() => {
+    if (params?.furniID === undefined) {
+      return;
+    }
+    const furniID = Number(params.furniID);
+    fetchFurniture.fetch({ ids: [furniID] });
+  }, [params?.furniID]);
+
+  const defaultInputValue = useMemo<any>(() => {
+    if (params?.furniID === undefined) {
+      return undefined;
+    }
+
+    const furniID = Number(params.furniID);
+    const matchingFurni = fetchFurniture.data?.[0];
+    if (matchingFurni?.id !== furniID) {
+      return undefined;
+    }
+
+    return matchingFurni.publicName
+  }, [params?.furniID, fetchFurniture.data?.[0]?.id]);
 
   const onSearchRares = async (query: string) => {
     const response = await fetchFurniture.fetch({ itemName: query, limit: 10 });
@@ -21,13 +44,13 @@ export function SearchRares() {
     }))
   }
 
-  const onSelectRare = (newFurni: FurnitureFragment) => {
-    setLocation(`/values/${newFurni.id}`);
+  const onSelectRare = (newFurni: { label: string, value: number }) => {
+    setLocation(`/values/${newFurni.value}`);
   }
 
   return (
     <div style={{ width: 400 }}>
-      <Select cacheOptions defaultOptions loadOptions={onSearchRares} onChange={onSelectRare as any} />
+      <Select cacheOptions defaultOptions inputValue={defaultInputValue} loadOptions={onSearchRares} onChange={onSelectRare as any} />
     </div>
   )
 }
--- apps/imagine-web/src/screens/values-view-item-screen/ValuesViewItemScreen.tsx
+++ apps/imagine-web/src/screens/values-view-item-screen/ValuesViewItemScreen.tsx
@@ -3,6 +3,7 @@ import React, { useEffect } from 'react';
 import { useFurnitureFetchOne } from '@imagine-cms/client';
 import { GridLarge } from '../../components/grid/Grid.remix';
 import { UsersWithFurniCard } from './users-with-furni-card/UsersWithFurniCard';
+import { SearchRares } from '../values-overview-screen/search-rares/SearchRares';
 import { FurnitureCommentListCard } from './furniture-comment-list-card/FurnitureCommentListCard';
 import { FurniturePriceTrendsCard } from './furniture-price-trends-card/FurniturePriceTrendsCard';
 import { FurniturePurchaseTrendsCard } from './furniture-purchase-trends-card/FurniturePurchaseTrendsCard';
@@ -20,7 +21,11 @@ export function ValuesViewItemScreen() {
 
   return (
     <>
-      <h1><Link to="/values"><i className="fa fa-caret-left" style={{ marginRight: 8 }} /></Link>Rare Values - {fetchFurniture.data?.publicName ?? <>#{furnitureID}</>}</h1>
+      <div style={{ display: 'flex', flex: 1, justifyContent: 'space-between' }}>
+
+        <h1><Link to="/values"><i className="fa fa-caret-left" style={{ marginRight: 8 }} /></Link>Rare Values - {fetchFurniture.data?.publicName ?? <>#{furnitureID}</>}</h1>
+        <SearchRares />
+      </div>
       <br />
       <GridLarge>
         {fetchFurniture.data ? <FurnitureValueGridContainer furniture={fetchFurniture.data} showViewMore={false} /> : <FurnitureValueGridContainerMock showViewMore={false} />}
