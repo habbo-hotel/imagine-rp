f707b449aeb1554bc361ab74f649fcd6ab414cef
chrismpettyjohn <chrismpettyjohn@gmail.com>
2023-12-24 15:51:01 -0500

    design: corp view screen

--- apps/imagine-web/src/components/corp-grid-container/CorpGridContainer.tsx
+++ apps/imagine-web/src/components/corp-grid-container/CorpGridContainer.tsx
@@ -2,16 +2,19 @@ import React, { useContext } from 'react';
 import { CorpGridContainerProps } from './CorpGridContainer.types';
 import { CorpGridContainerBadge, CorpGridContainerElement, CorpGridContainerInfo } from './CorpGridContainer.styled';
 import { configContext } from '@imagine-cms/web';
+import { Link } from 'wouter';
 
 export function CorpGridContainer({ corporation }: CorpGridContainerProps) {
   const { config } = useContext(configContext);
   return (
-    <CorpGridContainerElement>
-      <CorpGridContainerBadge src={`${config!.badgeURL}/${corporation.badgeCode}.${config!.badgeEXT}`} />
-      <CorpGridContainerInfo>
-        <h2>{corporation.name}</h2>
-        <p>{corporation.description}</p>
-      </CorpGridContainerInfo>
-    </CorpGridContainerElement>
+    <Link to={`/corps/${corporation.id}`}>
+      <CorpGridContainerElement>
+        <CorpGridContainerBadge src={`${config!.badgeURL}/${corporation.badgeCode}.${config!.badgeEXT}`} />
+        <CorpGridContainerInfo>
+          <h2>{corporation.name}</h2>
+          <p>{corporation.description}</p>
+        </CorpGridContainerInfo>
+      </CorpGridContainerElement>
+    </Link>
   );
 }
--- apps/imagine-web/src/screens/corp-view-screen/CorpViewScreen.tsx
+++ apps/imagine-web/src/screens/corp-view-screen/CorpViewScreen.tsx
@@ -1,9 +1,59 @@
-import React from 'react';
+import { useCorporationFetchOne } from '@imagine-cms/client';
+import React, { useContext, useEffect } from 'react';
+import { Link, useRoute } from 'wouter';
+import { GridLarge } from '../../components/grid/Grid.remix';
+import { configContext } from '@imagine-cms/web';
+import { CorpGridContainerBadge } from '../../components/corp-grid-container/CorpGridContainer.styled';
+import DayJS from 'dayjs';
+import { Card } from '../../components/card/Card';
 
 export function CorpViewScreen() {
+  const { config } = useContext(configContext);
+  const [, params] = useRoute<{ corpID: string }>('/corps/:corpID');
+  const corpID = Number(params!.corpID);
+  const fetchCorp = useCorporationFetchOne();
+
+  useEffect(() => {
+    fetchCorp.fetch({ id: corpID });
+  }, [corpID]);
+
   return (
     <>
-      CorpViewScreen
+      <div style={{ display: 'flex', flex: 1, gap: '1.4rem', marginBottom: '2rem' }}>
+        <Link to="/corps">
+          <i className="fa fa-caret-left fa-4x" style={{ cursor: 'pointer' }} />
+        </Link>
+        <div style={{ display: 'flex', flex: 1, gap: '1.4rem', alignItems: 'center' }}>
+          <CorpGridContainerBadge src={`${config!.badgeURL}/${fetchCorp.data?.badgeCode}.${config!.badgeEXT}`} />
+          <div>
+            <h4 style={{ margin: 0 }}>Corporations - Viewing:</h4>
+            <h1 style={{ margin: 0, fontWeight: 'bold' }}>{fetchCorp.data?.name ?? `#${corpID}`}</h1>
+          </div>
+        </div>
+      </div>
+      <GridLarge>
+        <div style={{ display: 'flex', flex: 1, flexDirection: 'column', gap: 16 }}>
+          <h2>About</h2>
+          <Card>
+            <div>
+              <h4>Founded:</h4>
+              <h3>{DayJS().format('MMMM DD, YYYY')}</h3>
+            </div>
+
+            <div>
+              <h4>Owned By:</h4>
+              <p>Me</p>
+            </div>
+            <div>
+              <h4>Based in:</h4>
+              <p>Detroit</p>
+            </div>
+          </Card>
+        </div>
+        <div style={{ flex: 1 }}>
+          <h2>Members</h2>
+        </div>
+      </GridLarge>
     </>
   )
 }
