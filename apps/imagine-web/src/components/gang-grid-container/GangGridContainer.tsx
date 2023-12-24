88710f4954723024a41899a870bc499226c04ee5
chrismpettyjohn <chrismpettyjohn@gmail.com>
2023-12-24 16:10:32 -0500

    design: gang view page

--- apps/imagine-web/src/components/gang-grid-container/GangGridContainer.tsx
+++ apps/imagine-web/src/components/gang-grid-container/GangGridContainer.tsx
@@ -2,18 +2,20 @@ import React from 'react';
 import { GangGridContainerAvatar, GangGridContainerElement, GangGridContainerInfo } from './GangGridContainer.styled';
 import { GangGridContainerProps } from './GangGridContainer.types';
 import { Avatar } from '../avatar/Avatar';
+import { Link } from 'wouter';
 
 export function GangGridContainer({ gang }: GangGridContainerProps) {
-  console.log(gang)
   return (
-    <GangGridContainerElement>
-      <GangGridContainerAvatar>
-        <Avatar look={gang.user.look} />
-      </GangGridContainerAvatar>
-      <GangGridContainerInfo>
-        <h2>{gang.name}</h2>
-        <p>{gang.description}</p>
-      </GangGridContainerInfo>
-    </GangGridContainerElement>
+    <Link to={`/gangs/${gang.id}`}>
+      <GangGridContainerElement>
+        <GangGridContainerAvatar>
+          <Avatar look={gang.user.look} />
+        </GangGridContainerAvatar>
+        <GangGridContainerInfo>
+          <h2>{gang.name}</h2>
+          <p>{gang.description}</p>
+        </GangGridContainerInfo>
+      </GangGridContainerElement>
+    </Link>
   )
 }
--- apps/imagine-web/src/screens/gang-view-screen/GangViewScreen.tsx
+++ apps/imagine-web/src/screens/gang-view-screen/GangViewScreen.tsx
@@ -1,9 +1,63 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { Link, useRoute } from 'wouter';
+import { GridLarge } from '../../components/grid/Grid.remix';
+import DayJS from 'dayjs';
+import { Card } from '../../components/card/Card';
+import { useGangFetchOne } from '@imagine-cms/client';
+import { GangGridContainerAvatar } from '../../components/gang-grid-container/GangGridContainer.styled';
+import { Avatar } from '../../components/avatar/Avatar';
 
 export function GangViewScreen() {
+  const [, params] = useRoute<{ gangID: string }>('/gangs/:gangID');
+  const gangID = Number(params!.gangID);
+  const fetchGang = useGangFetchOne();
+
+  console.log(fetchGang)
+
+  useEffect(() => {
+    fetchGang.fetch({ id: gangID });
+  }, [gangID]);
+
   return (
     <>
-      GangViewScreen
+      <div style={{ display: 'flex', flex: 1, gap: '1.4rem', marginBottom: '2rem' }}>
+        <Link to="/gangs">
+          <i className="fa fa-caret-left fa-4x" style={{ cursor: 'pointer' }} />
+        </Link>
+        <div style={{ display: 'flex', flex: 1, gap: '1.4rem', alignItems: 'center' }}>
+          <GangGridContainerAvatar>
+            {fetchGang.data &&
+              <Avatar look={fetchGang.data.user.look} />}
+          </GangGridContainerAvatar>
+          <div>
+            <h4 style={{ margin: 0 }}>Gangs - Viewing:</h4>
+            <h1 style={{ margin: 0, fontWeight: 'bold' }}>{fetchGang.data?.name ?? `#${gangID}`}</h1>
+          </div>
+        </div>
+      </div>
+      <GridLarge>
+        <div style={{ display: 'flex', flex: 1, flexDirection: 'column', gap: 16 }}>
+          <h2>About</h2>
+          <Card>
+            <div>
+              <h4>Founded:</h4>
+              <h3>{DayJS().format('MMMM DD, YYYY')}</h3>
+            </div>
+
+            <div>
+              <h4>Owned By:</h4>
+              <p>Me</p>
+            </div>
+            <div>
+              <h4>Based in:</h4>
+              <p>Detroit</p>
+            </div>
+          </Card>
+        </div>
+        <div style={{ flex: 1 }}>
+          <h2>Members</h2>
+        </div>
+      </GridLarge>
     </>
   )
 }
