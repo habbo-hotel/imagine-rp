e372daf19318bc955e17568e40db32791127284f
chrismpettyjohn <chrismpettyjohn@gmail.com>
2023-12-24 15:33:04 -0500

    design: improve corp container styling

--- apps/imagine-web/src/components/corp-grid-container/CorpGridContainer.tsx
+++ apps/imagine-web/src/components/corp-grid-container/CorpGridContainer.tsx
@@ -1,17 +1,17 @@
 import React, { useContext } from 'react';
 import { CorpGridContainerProps } from './CorpGridContainer.types';
-import { CorpGridContainerElement } from './CorpGridContainer.styled';
+import { CorpGridContainerBadge, CorpGridContainerElement, CorpGridContainerInfo } from './CorpGridContainer.styled';
 import { configContext } from '@imagine-cms/web';
 
 export function CorpGridContainer({ corporation }: CorpGridContainerProps) {
   const { config } = useContext(configContext);
   return (
     <CorpGridContainerElement>
-      <img src={`${config!.badgeURL}/${corporation.badgeCode}.${config!.badgeEXT}`} />
-      <div>
+      <CorpGridContainerBadge src={`${config!.badgeURL}/${corporation.badgeCode}.${config!.badgeEXT}`} />
+      <CorpGridContainerInfo>
         <h2>{corporation.name}</h2>
         <p>{corporation.description}</p>
-      </div>
+      </CorpGridContainerInfo>
     </CorpGridContainerElement>
   );
 }
