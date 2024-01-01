8149e156f34ae384b32726fc5ee558952737ddb8
chrismpettyjohn <chrismpettyjohn@gmail.com>
2023-12-31 21:14:41 -0500

    feat: virtual table for efficient rp stats querying

--- apps/imagine-api/src/database/rp-stats.entity.ts
+++ apps/imagine-api/src/database/rp-stats.entity.ts
@@ -1,43 +1,43 @@
-import {Column, Entity, PrimaryColumn, PrimaryGeneratedColumn} from 'typeorm';
+import { Column, Entity, PrimaryColumn, PrimaryGeneratedColumn } from 'typeorm';
 
-@Entity('rp_stats')
+@Entity('view_rp_stats')
 export class RPStatsEntity {
   @PrimaryGeneratedColumn()
   id!: number;
 
-  @Column({name: 'health_current'})
+  @Column({ name: 'health_current' })
   healthCurrent!: number;
 
-  @Column({name: 'health_max'})
+  @Column({ name: 'health_max' })
   healthMax!: number;
 
-  @Column({name: 'hunger_current'})
+  @Column({ name: 'hunger_current' })
   hungerCurrent!: number;
 
-  @Column({name: 'hunger_max'})
+  @Column({ name: 'hunger_max' })
   hungerMax!: number;
 
-  @Column({name: 'energy_current'})
+  @Column({ name: 'energy_current' })
   energyCurrent!: number;
 
-  @Column({name: 'energy_max'})
+  @Column({ name: 'energy_max' })
   energyMax!: number;
 
-  @Column({name: 'armor_current'})
+  @Column({ name: 'armor_current' })
   armorCurrent!: number;
 
-  @Column({name: 'armor_max'})
+  @Column({ name: 'armor_max' })
   armorMax!: number;
 
-  @Column({name: 'corporation_id'})
+  @Column({ name: 'corp_id' })
   corporationID?: number;
 
-  @Column({name: 'corporation_rank_id'})
+  @Column({ name: 'corp_id' })
   corporationRankID?: number;
 
-  @Column({name: 'gang_id'})
+  @Column({ name: 'gang_id' })
   gangID?: number;
 
-  @Column({name: 'gang_rank_id'})
+  @Column({ name: 'gang_rank_id' })
   gangRankID?: number;
 }
