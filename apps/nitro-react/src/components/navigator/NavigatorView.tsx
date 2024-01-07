17f8c5dfa3fd2c599f873480d407d52048f6a7cd
habbo-hotel <chrismpettyjohn999@gmail.com>
2024-01-07 00:00:05 -0500

    feat: clean up currency display

--- apps/nitro-react/src/components/navigator/NavigatorView.tsx
+++ apps/nitro-react/src/components/navigator/NavigatorView.tsx
@@ -12,28 +12,25 @@ import { NavigatorRoomSettingsView } from './views/room-settings/NavigatorRoomSe
 import { NavigatorSearchResultView } from './views/search/NavigatorSearchResultView';
 import { NavigatorSearchView } from './views/search/NavigatorSearchView';
 
-export const NavigatorView: FC<{}> = props =>
-{
-    const [ isVisible, setIsVisible ] = useState(false);
-    const [ isReady, setIsReady ] = useState(false);
-    const [ isCreatorOpen, setCreatorOpen ] = useState(false);
-    const [ isRoomInfoOpen, setRoomInfoOpen ] = useState(false);
-    const [ isRoomLinkOpen, setRoomLinkOpen ] = useState(false);
-    const [ isLoading, setIsLoading ] = useState(false);
-    const [ needsInit, setNeedsInit ] = useState(true);
-    const [ needsSearch, setNeedsSearch ] = useState(false);
+export const NavigatorView: FC<{}> = props => {
+    const [isVisible, setIsVisible] = useState(false);
+    const [isReady, setIsReady] = useState(false);
+    const [isCreatorOpen, setCreatorOpen] = useState(false);
+    const [isRoomInfoOpen, setRoomInfoOpen] = useState(false);
+    const [isRoomLinkOpen, setRoomLinkOpen] = useState(false);
+    const [isLoading, setIsLoading] = useState(false);
+    const [needsInit, setNeedsInit] = useState(true);
+    const [needsSearch, setNeedsSearch] = useState(false);
     const { searchResult = null, topLevelContext = null, topLevelContexts = null, navigatorData = null } = useNavigator();
     const pendingSearch = useRef<{ value: string, code: string }>(null);
     const elementRef = useRef<HTMLDivElement>();
 
-    useRoomSessionManagerEvent<RoomSessionEvent>(RoomSessionEvent.CREATED, event =>
-    {
+    useRoomSessionManagerEvent<RoomSessionEvent>(RoomSessionEvent.CREATED, event => {
         setIsVisible(false);
         setCreatorOpen(false);
     });
 
-    const sendSearch = useCallback((searchValue: string, contextCode: string) =>
-    {
+    const sendSearch = useCallback((searchValue: string, contextCode: string) => {
         setCreatorOpen(false);
 
         SendMessageComposer(new NavigatorSearchComposer(contextCode, searchValue));
@@ -41,17 +38,14 @@ export const NavigatorView: FC<{}> = props =>
         setIsLoading(true);
     }, []);
 
-    const reloadCurrentSearch = useCallback(() =>
-    {
-        if(!isReady)
-        {
+    const reloadCurrentSearch = useCallback(() => {
+        if (!isReady) {
             setNeedsSearch(true);
 
             return;
         }
 
-        if(pendingSearch.current)
-        {
+        if (pendingSearch.current) {
             sendSearch(pendingSearch.current.value, pendingSearch.current.code);
 
             pendingSearch.current = null;
@@ -59,29 +53,25 @@ export const NavigatorView: FC<{}> = props =>
             return;
         }
 
-        if(searchResult)
-        {
+        if (searchResult) {
             sendSearch(searchResult.data, searchResult.code);
 
             return;
         }
 
-        if(!topLevelContext) return;
+        if (!topLevelContext) return;
 
         sendSearch('', topLevelContext.code);
-    }, [ isReady, searchResult, topLevelContext, sendSearch ]);
+    }, [isReady, searchResult, topLevelContext, sendSearch]);
 
-    useEffect(() =>
-    {
+    useEffect(() => {
         const linkTracker: ILinkEventTracker = {
-            linkReceived: (url: string) =>
-            {
+            linkReceived: (url: string) => {
                 const parts = url.split('/');
-        
-                if(parts.length < 2) return;
-        
-                switch(parts[1])
-                {
+
+                if (parts.length < 2) return;
+
+                switch (parts[1]) {
                     case 'show': {
                         setIsVisible(true);
                         setNeedsSearch(true);
@@ -91,13 +81,12 @@ export const NavigatorView: FC<{}> = props =>
                         setIsVisible(false);
                         return;
                     case 'toggle': {
-                        if(isVisible)
-                        {
+                        if (isVisible) {
                             setIsVisible(false);
-        
+
                             return;
                         }
-        
+
                         setIsVisible(true);
                         setNeedsSearch(true);
                         return;
@@ -109,18 +98,17 @@ export const NavigatorView: FC<{}> = props =>
                         setRoomLinkOpen(value => !value);
                         return;
                     case 'goto':
-                        if(parts.length <= 2) return;
-        
-                        switch(parts[2])
-                        {
+                        if (parts.length <= 2) return;
+
+                        switch (parts[2]) {
                             case 'home':
-                                if(navigatorData.homeRoomId <= 0) return;
-        
+                                if (navigatorData.homeRoomId <= 0) return;
+
                                 TryVisitRoom(navigatorData.homeRoomId);
                                 break;
                             default: {
                                 const roomId = parseInt(parts[2]);
-        
+
                                 TryVisitRoom(roomId);
                             }
                         }
@@ -130,16 +118,15 @@ export const NavigatorView: FC<{}> = props =>
                         setCreatorOpen(true);
                         return;
                     case 'search':
-                        if(parts.length > 2)
-                        {
+                        if (parts.length > 2) {
                             const topLevelContextCode = parts[2];
-        
+
                             let searchValue = '';
-        
-                            if(parts.length > 3) searchValue = parts[3];
-        
+
+                            if (parts.length > 3) searchValue = parts[3];
+
                             pendingSearch.current = { value: searchValue, code: topLevelContextCode };
-        
+
                             setIsVisible(true);
                             setNeedsSearch(true);
                         }
@@ -152,81 +139,72 @@ export const NavigatorView: FC<{}> = props =>
         AddEventLinkTracker(linkTracker);
 
         return () => RemoveLinkEventTracker(linkTracker);
-    }, [ isVisible, navigatorData ]);
+    }, [isVisible, navigatorData]);
 
-    useEffect(() =>
-    {
-        if(!searchResult) return;
+    useEffect(() => {
+        if (!searchResult) return;
 
         setIsLoading(false);
 
-        if(elementRef && elementRef.current) elementRef.current.scrollTop = 0;
-    }, [ searchResult ]);
+        if (elementRef && elementRef.current) elementRef.current.scrollTop = 0;
+    }, [searchResult]);
 
-    useEffect(() =>
-    {
-        if(!isVisible || !isReady || !needsSearch) return;
+    useEffect(() => {
+        if (!isVisible || !isReady || !needsSearch) return;
 
         reloadCurrentSearch();
 
         setNeedsSearch(false);
-    }, [ isVisible, isReady, needsSearch, reloadCurrentSearch ]);
+    }, [isVisible, isReady, needsSearch, reloadCurrentSearch]);
 
-    useEffect(() =>
-    {
-        if(isReady || !topLevelContext) return;
+    useEffect(() => {
+        if (isReady || !topLevelContext) return;
 
         setIsReady(true);
-    }, [ isReady, topLevelContext ]);
+    }, [isReady, topLevelContext]);
 
-    useEffect(() =>
-    {
-        if(!isVisible || !needsInit) return;
+    useEffect(() => {
+        if (!isVisible || !needsInit) return;
 
         SendMessageComposer(new NavigatorInitComposer());
 
         setNeedsInit(false);
-    }, [ isVisible, needsInit ]);
+    }, [isVisible, needsInit]);
 
-    useEffect(() =>
-    {
-        LegacyExternalInterface.addCallback(HabboWebTools.OPENROOM, (k: string, _arg_2: boolean = false, _arg_3: string = null) => SendMessageComposer(new ConvertGlobalRoomIdMessageComposer(k)));
-    }, []);
 
     return (
         <>
-            { isVisible &&
+            {isVisible &&
                 <NitroCardView uniqueKey="navigator" className="nitro-navigator">
-                    <NitroCardHeaderView headerText={ LocalizeText(isCreatorOpen ? 'navigator.createroom.title' : 'navigator.title') } onCloseClick={ event => setIsVisible(false) } />
+                    <NitroCardHeaderView headerText={LocalizeText(isCreatorOpen ? 'navigator.createroom.title' : 'navigator.title')} onCloseClick={event => setIsVisible(false)} />
                     <NitroCardTabsView>
-                        { topLevelContexts && (topLevelContexts.length > 0) && topLevelContexts.map((context, index) =>
-                        {
+                        {topLevelContexts && (topLevelContexts.length > 0) && topLevelContexts.map((context, index) => {
                             return (
-                                <NitroCardTabsItemView key={ index } isActive={ ((topLevelContext === context) && !isCreatorOpen) } onClick={ event => sendSearch('', context.code) }>
-                                    { LocalizeText(('navigator.toplevelview.' + context.code)) }
+                                <NitroCardTabsItemView key={index} isActive={((topLevelContext === context) && !isCreatorOpen)} onClick={event => sendSearch('', context.code)}>
+                                    {LocalizeText(('navigator.toplevelview.' + context.code))}
                                 </NitroCardTabsItemView>
                             );
-                        }) }
-                        <NitroCardTabsItemView isActive={ isCreatorOpen } onClick={ event => setCreatorOpen(true) }>
+                        })}
+                        <NitroCardTabsItemView isActive={isCreatorOpen} onClick={event => setCreatorOpen(true)}>
                             <FaPlus className="fa-icon" />
                         </NitroCardTabsItemView>
                     </NitroCardTabsView>
                     <NitroCardContentView position="relative">
-                        { isLoading &&
-                            <Base fit position="absolute" className="top-0 start-0 z-index-1 bg-muted opacity-0-5" /> }
-                        { !isCreatorOpen &&
+                        {isLoading &&
+                            <Base fit position="absolute" className="top-0 start-0 z-index-1 bg-muted opacity-0-5" />}
+                        {!isCreatorOpen &&
                             <>
-                                <NavigatorSearchView sendSearch={ sendSearch } />
-                                <Column innerRef={ elementRef } overflow="auto">
-                                    { (searchResult && searchResult.results.map((result, index) => <NavigatorSearchResultView key={ index } searchResult={ result } />)) }
+                                <NavigatorSearchView sendSearch={sendSearch} />
+                                <Column innerRef={elementRef} overflow="auto">
+                                    {(searchResult && searchResult.results.map((result, index) => <NavigatorSearchResultView key={index} searchResult={result} />))}
                                 </Column>
-                            </> }
-                        { isCreatorOpen && <NavigatorRoomCreatorView /> }
+                            </>}
+                        {isCreatorOpen && <NavigatorRoomCreatorView />}
                     </NitroCardContentView>
-                </NitroCardView> }
+                </NitroCardView>}
             <NavigatorDoorStateView />
-            { isRoomInfoOpen && <NavigatorRoomInfoView onCloseClick={ () => setRoomInfoOpen(false) } /> }
-            { isRoomLinkOpen && <NavigatorRoomLinkView onCloseClick={ () => setRoomLinkOpen(false) } /> }
+            {isRoomInfoOpen && <NavigatorRoomInfoView onCloseClick={() => setRoomInfoOpen(false)} />}
+            {isRoomLinkOpen && <NavigatorRoomLinkView onCloseClick={() => setRoomLinkOpen(false)} />}
             <NavigatorRoomSettingsView />
         </>
     );
