7704469407186d8898b0a08bc769ff820aefefbb
habbo-hotel <chrismpettyjohn999@gmail.com>
2024-01-07 23:28:04 -0500

    feat: support font awesome

--- apps/imagine-next/src/app/layout.tsx
+++ apps/imagine-next/src/app/layout.tsx
@@ -1,4 +1,5 @@
 import 'dotenv/config';
+import './fontawesome.css';
 import Head from 'next/head';
 import type { Metadata } from 'next'
 import { ThemeProvider } from '../site-ui/theme/ThemeProvider';
@@ -19,28 +20,30 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+
+    <>
       <Head>
-        <link rel="stylesheet" href="/css/fontawesome.css" />
+        <link rel="stylesheet" href="/css/fontawesome.css" type="text/css" />
         <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@400;500;600;700&display=swap" rel="stylesheet" />
       </Head>
-      <body>
-        <ImagineContextProviders loadingScreen={<LoadingScreen />}>
-          <WebsocketContextProvider>
-            <UsersOnlineContextProvider>
-              <ThemeProvider>
-                <SiteBody />
-                <SiteContainer>
-                  <Transition>
+      <html lang="en">
+        <body>
+          <ImagineContextProviders loadingScreen={<LoadingScreen />}>
+            <WebsocketContextProvider>
+              <UsersOnlineContextProvider>
+                <ThemeProvider>
+                  <SiteBody />
+                  <SiteContainer>
+                    <Transition>
 
-                    {children}
-                  </Transition>
-                </SiteContainer>
-              </ThemeProvider>
-            </UsersOnlineContextProvider>
-          </WebsocketContextProvider>
-        </ImagineContextProviders>
-      </body>
-    </html>
+                      {children}
+                    </Transition>
+                  </SiteContainer>
+                </ThemeProvider>
+              </UsersOnlineContextProvider>
+            </WebsocketContextProvider>
+          </ImagineContextProviders>
+        </body>
+      </html></>
   )
 }
